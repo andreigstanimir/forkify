d162bab8290666329d04e2ba2a5df22413b94128
Andrei G. Stanimir <andreigstanimir@gmailcom>
2023-07-04 23:03:25 +0300

    added fracty instead of fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import icons from '../../img/icons.svg';
 import View from './View.js';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -127,9 +127,7 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
-      }
+      <div class="recipe__quantity">${ing.quantity ? fracty(ing.quantity) : ''}
       </div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
